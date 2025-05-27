f5f2fb5bb329339699a9d6810bf8697ea09885d6
Smith0212 <smitsutariya056@gmail.com>
2025-05-27 19:25:32 +0530

    API url changed in frontend

--- backend/app.js
+++ backend/app.js
@@ -30,7 +30,7 @@ app.use(cors({
     credentials: true,
     optionsSuccessStatus: 200,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'Cookie']
+    allowedHeaders: ['Content-Type', 'Authorization', "token", "accept-language", 'Cookie']
 }));
 
 
